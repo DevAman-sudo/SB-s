bf23632a439ac8af850c0ec038b1b8344959f6ba
spydev <spydev@kali.spydev>
2022-03-14 00:33:48 +0530

    animations

---
+++ public/js/main.js
@@ -0,0 +1,14 @@
+anime({
+    targets: '.pop',
+    opacity: [0, 1],
+    easing: "linear",
+    duration: 3000
+});
+
+anime({
+    targets: '.slide',
+    translateX: 10,
+    opacity: [0, 1],
+    easing: "linear",
+    duration: 3000
+});
